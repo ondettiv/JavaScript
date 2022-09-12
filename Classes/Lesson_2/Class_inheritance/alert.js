206a06ad891c8957b80d08119b48990bdc1389cf
Vladimir Ondetti <vladimir.ondetti@Vladimirs-MBP.fibertel.com.ar>
2022-09-12 16:51:33 -0300

    Classes

---
+++ Classes/Lesson_2/Class_inheritance/alert.js
@@ -0,0 +1,22 @@
+// Here’s the code with Rabbit extending Animal.
+
+// Unfortunately, Rabbit objects can’t be created. What’s wrong? Fix it.
+
+class Animal {
+
+  constructor(name) {
+    this.name = name;
+  }
+
+}
+
+class Rabbit extends Animal {
+  constructor(name) {
+    super(name);
+    this.name = name;
+    this.created = Date.now();
+  }
+}
+
+let rabbit = new Rabbit("White Rabbit"); // Error: this is not defined
+alert(rabbit.name);
